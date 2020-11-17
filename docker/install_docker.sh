1124d31f3fa94b92c568bc2cecd201c3205dcbff
wm-efren-carles <efrencalres@worldteamproject.com>
2020-11-17 18:39:57 +0100

    tor installation script, we hope it's just temporary

---
+++ docker/install_docker.sh
@@ -0,0 +1,14 @@
+export DEBIAN_FRONTEND=noninteractive
+sudo apt update
+sudo apt-get install -y git build-essential automake libevent-dev libssl-dev zlib1g-dev python3.8 pkg-config liblzma-dev libzstd-dev
+git clone https://gitlab.com/torproject/tor.git
+cp config/auths_dir.inc tor/src/app/config/auth_dirs.inc
+cd /home/ubuntu/PTN/docker/tor && ./autogen.sh
+cd /home/ubuntu/PTN/docker/tor && ./configure --disable-asciidoc && make
+cd /home/ubuntu/PTN/docker/tor && make install
+cd /home/ubuntu/PTN/docker && sudo cp config/torrc_auth /usr/local/etc/tor/torrc
+mkdir /home/ubuntu/log
+mkdir /home/ubuntu/.tor
+mkdir /home/ubuntu/.tor/keys
+cd /home/ubuntu/.tor/keys && cat test | tor-gencert -v --create-identity-key --passphrase-fd test
+
