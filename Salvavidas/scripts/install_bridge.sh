b06357c1d589983983cf3548e03376c09015982d
wm-efren-carles <efrencalres@worldteamproject.com>
2020-11-25 17:41:19 +0100

    New files, env vars and more

--- Salvavidas/scripts/install_bridge.sh
+++ Salvavidas/scripts/install_bridge.sh
@@ -1,7 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update
 sudo apt-get install -y git obfs4proxy
-sudo apt-get install tor
-sudo setcap cap_net_bind_service=+ep /user/bin/obfs4proxy
-cd ../config && sudo cp torrc_bridge /usr/local/etc/tor/torrc
+sudo apt-get install -y tor
+sudo setcap cap_net_bind_service=+ep /usr/bin/obfs4proxy
+cd ../config && sudo cp torrc_bridge /etc/tor/torrc
 
--- Salvavidas/terraform/torbridge.tf
+++ Salvavidas/terraform/torbridge.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -18,12 +17,47 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+
+
 resource "aws_instance" "torbridge" {
-	ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name = "prova-node"
+  count           = var.COUNT
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.MACHINE_TYPE
   security_groups = ["test-all-allowed"]
   tags = {
     Name = "TorBridge"
   }
+
+}
+
+
+resource "aws_security_group" "torbridge_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+
+output "ips" {
+  value = [aws_instance.torbridge.public_ip]
 }
