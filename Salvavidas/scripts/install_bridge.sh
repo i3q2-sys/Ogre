a8f0cb9471de498c5aebb23feea03f96d7659d52
wm-efren-carles <efrencalres@worldteamproject.com>
2020-11-26 17:21:41 +0100

    fixed script

--- Salvavidas/scripts/install_bridge.sh
+++ Salvavidas/scripts/install_bridge.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt-get install -y git obfs4proxy
 sudo apt-get install -y tor
 sudo setcap cap_net_bind_service=+ep /usr/bin/obfs4proxy
-cd ../config && sudo cp torrc_bridge /etc/tor/torrc
-
+sudo cp ~/torrc_bridge /etc/tor/torrc
+nohup tor &
