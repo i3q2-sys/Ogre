cf509984cd1915007e50c6fb9841f65ae7fb7d75
wm-efren-carles <efrencalres@worldteamproject.com>
2020-11-29 19:03:05 +0100

    trash outt

--- Salvavidas/scripts/install_bridge.sh
+++
@@ -1,8 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-sudo apt update
-sudo apt-get install -y git obfs4proxy
-sudo apt-get install -y tor
-sudo setcap cap_net_bind_service=+ep /usr/bin/obfs4proxy
-sudo cp ~/torrc_bridge /etc/tor/torrc
-sudo systemctl stop tor
-nohup tor &
